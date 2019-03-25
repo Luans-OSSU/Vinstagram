0e81c900883b7bed02307628c44bdc8fa32132d7
Luans <luansnain@gmail.com>
2019-03-25 21:24:07 +0100

    init, added scss, posts

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="app-phone">
+      <div class="phone-header">
+        <span class="title">Vinstagram</span>
+      </div>
+      <phone-body
+        :step="step"
+        :posts="posts"
+        :filters="filters"
+        :image="image"
+        :selectedFilter="selectedFilter"
+        v-model="caption"
+      />
+      <div class="phone-footer">
+        <div class="home-cta">
+          <i class="fas fa-home fa-lg"></i>
+        </div>
+        <div class="upload-cta">
+          <i class="fas fa-plus-square fa-lg"></i>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PhoneBody from "./components/PhoneBody";
+
+import posts from "./data/posts.js";
+import filters from "./data/filter.js";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      step: 1,
+      posts,
+      filters,
+      image: "",
+      selectedFilter: "",
+      caption: ""
+    }
+  },
   components: {
-    HelloWorld
+    "phone-body": PhoneBody
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" src="./styles/app.scss">
 </style>
---
+++ src/components/PhoneBody.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="phone-body">
+       <div class="feed">
+           <vinstagram-post
+                v-for="post in posts"
+                :post="post"
+                :key="posts.indexOf(post)"
+           >
+           </vinstagram-post>
+       </div>
+    </div>
+</template>
+
+<script>
+
+import VisntagramPost from "./VinstagramPost";
+
+export default {
+    name: "PhoneBody",
+    props: {
+        posts: Array,
+        filters: Array
+    },
+    components: {
+        "vinstagram-post": VisntagramPost
+    }
+}
+
+</script>
+
+
+<style lang="scss" src="../styles/phone-body.scss"></style>
